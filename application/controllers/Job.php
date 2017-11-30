1d8e0e843036d8d003244bbab3c5724468e72bc9
Niko Arellano <narellano@my.bcit.ca>
2017-11-30 10:31:00 -0800

    setup restful controller

---
+++ application/controllers/Job.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * REST server for ferry schedule operations.
+ * This one manages ports.
+ *
+ * ------------------------------------------------------------------------
+ */
+require APPPATH . '/third_party/restful/libraries/Rest_controller.php';
+
+class Job extends Rest_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+    }
+    
+	// Handle an incoming GET ... 	returns a list of ports
+	function index_get()
+	{
+		$this->response($this->tasks->all(), 200);
+	}
+	// The other REST methods are not handled, since we are not doing maintenance
+}
